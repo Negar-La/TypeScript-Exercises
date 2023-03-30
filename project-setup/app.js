050655d46cd7144cd45965002ae54aa6d2db919f
Negar Lashgari <negar.lashgari@gmail.com>
2023-03-30 16:56:22 -0400

    type Inference

--- project-setup/app.js
+++ project-setup/app.js
@@ -1,10 +1,16 @@
-var add = function (n1, n2) {
+//JS uses dynamic types (resolved at runtime) TS uses static types (set during development)
+var add = function (n1, n2, print, phrase) {
     // if (typeof n1 !== "number" || typeof n2 !== "number") {
     //   throw new Error("Incorrect input!!!");
-    // } 
-    return n1 + n2;
+    // }
+    var math = n1 + n2;
+    if (print) {
+        console.log(phrase + math);
+    }
+    return math;
 };
 var number1 = 5;
-var number2 = 2.6;
-var result = add(number1, number2);
-console.log(result, typeof result);
+var number2 = 2.7;
+var showResult = true;
+var resultPhrase = 'Result is ';
+add(number1, number2, showResult, resultPhrase);
