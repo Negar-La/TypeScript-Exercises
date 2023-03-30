9be4662803c994bbe364286ac45189c73c915a8f
Negar Lashgari <negar.lashgari@gmail.com>
2023-03-30 14:51:51 -0400

    JS vs TS types-dynamic-static

--- project-setup/app.js
+++ project-setup/app.js
@@ -1,1 +1,10 @@
-console.log("your code goesssss!...");
+var add = function (n1, n2) {
+    // if (typeof n1 !== "number" || typeof n2 !== "number") {
+    //   throw new Error("Incorrect input!!!");
+    // } 
+    return n1 + n2;
+};
+var number1 = 5;
+var number2 = 2.6;
+var result = add(number1, number2);
+console.log(result, typeof result);
--- project-setup/app.ts
+++ project-setup/app.ts
@@ -1,1 +1,12 @@
-console.log("your code goesssss!...");
+const add = (n1: number, n2: number) => {
+  // if (typeof n1 !== "number" || typeof n2 !== "number") {
+  //   throw new Error("Incorrect input!!!");
+  // } 
+    return n1 + n2;
+};
+
+const number1 = 5;
+const number2 = 2.6;
+
+const result = add(number1, number2);
+console.log(result, typeof result);
