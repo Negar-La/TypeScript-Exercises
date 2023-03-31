9b9e34b596c6fe439e49150ca2515a98f895854b
Negar Lashgari <negar.lashgari@gmail.com>
2023-03-31 12:49:20 -0400

    object type

--- project-setup/app.js
+++ project-setup/app.js
@@ -1,16 +1,9 @@
-//JS uses dynamic types (resolved at runtime) TS uses static types (set during development)
-var add = function (n1, n2, print, phrase) {
-    // if (typeof n1 !== "number" || typeof n2 !== "number") {
-    //   throw new Error("Incorrect input!!!");
-    // }
-    var math = n1 + n2;
-    if (print) {
-        console.log(phrase + math);
-    }
-    return math;
+// const person: {
+//     name: string;
+//     age: number
+// } = {
+var person = {
+    name: "negar",
+    age: 30
 };
-var number1 = 5;
-var number2 = 2.7;
-var showResult = true;
-var resultPhrase = 'Result is ';
-add(number1, number2, showResult, resultPhrase);
+console.log(person.name, person.age);
---
+++ project-setup/basics.ts
@@ -0,0 +1,21 @@
+//JS uses dynamic types (resolved at runtime) TS uses static types (set during development)
+const add = (n1: number, n2: number, print: boolean, phrase: string) => {
+  // if (typeof n1 !== "number" || typeof n2 !== "number") {
+  //   throw new Error("Incorrect input!!!");
+  // }
+
+const math = n1 + n2
+  if (print) {
+    console.log(phrase + math);
+  }
+
+    return math;
+};
+
+const number1 = 5;
+const number2 = 2.7;
+const showResult = true;
+const resultPhrase = 'Result is '
+
+ add(number1, number2, showResult, resultPhrase);
+
