8c80a60a4df6098ff2b828a127498cc9fcfcef31
Negar Lashgari <negar.lashgari@gmail.com>
2023-03-31 14:06:31 -0400

    working with tuple

--- project-setup/app.js
+++ project-setup/app.js
@@ -6,26 +6,36 @@ var person = {
     name: "negar",
     age: 30,
 };
+console.log(person.name, person.age);
+//-------------------------------------------------------------//
 var product = {
     id: "abc1",
     price: 12.99,
     tags: ["great-offer", "hot-and-new"],
     numbers: [1, 2, 3],
     data: [1, 2, "new", [3, "expensive"], { key: "On Sale" }],
+    role: [2, "author"],
     details: {
         title: "Red Carpet",
         description: "A great carpet - almost brand-new!",
     },
 };
-console.log(person.name, person.age);
 console.log(product.details.title);
-console.log(product.data[1]);
+console.log(product.data[2]);
 //a simple for loop in an array of strings
 for (var _i = 0, _a = product.tags; _i < _a.length; _i++) {
     var dat = _a[_i];
-    console.log(dat.toLocaleUpperCase());
+    console.log(dat.toLocaleUpperCase()); //we can use methods for strings
 }
+//a simple for loop in an array of numbers
 for (var _b = 0, _c = product.numbers; _b < _c.length; _b++) {
     var num = _c[_b];
-    console.log(num.toString());
+    console.log(num + typeof num + " converted to " + num.toString() + typeof num.toString());
 }
+//-------------------------------------------------------------//
+//working with Tuples
+product.role.push("writer"); //push method works with tuple! strange!
+console.log(product.role); //[2, 'author', 'writer']
+// product.role = [-5, 10]; //error
+// product.role[1] = 10;
+console.log(product.role); //[2, 10, "writer"];
--- project-setup/app.ts
+++ project-setup/app.ts
@@ -6,28 +6,62 @@ const person = {
   name: "negar",
   age: 30,
 };
+console.log(person.name, person.age);
 
-const product = {
+//-------------------------------------------------------------//
+
+const product: {
+  id: string;
+  price: number;
+  tags: string[];
+  numbers: number[];
+  data: (
+    | string
+    | number
+    | (string | number)[]
+    | {
+        key: string;
+      }
+  )[];
+  role: [number, string]; //its a tuple type!!!
+  details: {
+    title: string;
+    description: string;
+  };
+} = {
   id: "abc1",
   price: 12.99,
   tags: ["great-offer", "hot-and-new"],
   numbers: [1, 2, 3],
   data: [1, 2, "new", [3, "expensive"], { key: "On Sale" }],
+  role: [2, "author"],
   details: {
     title: "Red Carpet",
     description: "A great carpet - almost brand-new!",
   },
 };
 
-console.log(person.name, person.age);
 console.log(product.details.title);
-console.log(product.data[1]);
+console.log(product.data[2]);
 
 //a simple for loop in an array of strings
 for (const dat of product.tags) {
-  console.log(dat.toLocaleUpperCase());
+  console.log(dat.toLocaleUpperCase()); //we can use methods for strings
+}
+//a simple for loop in an array of numbers
+for (const num of product.numbers) {
+  console.log(
+    num + typeof num + " converted to " + num.toString() + typeof num.toString()
+  );
 }
 
-for (const num of product.numbers) {
-  console.log(num.toString());
-}
+//-------------------------------------------------------------//
+
+//working with Tuples
+product.role.push("writer"); //push method works with tuple! strange!
+console.log(product.role); //[2, 'author', 'writer']
+
+// product.role = [-5, 10]; //error
+
+// product.role[1] = 10;
+console.log(product.role); //[2, 10, "writer"];
