0900af43d91da3bb2fc189cddaa166c41bdab750
Negar Lashgari <negar.lashgari@gmail.com>
2023-03-31 16:23:41 -0400

    union types

--- project-setup/app.js
+++ project-setup/app.js
@@ -1,41 +1,14 @@
-// const person: {
-//     name: string;
-//     age: number
-// } = {
-var person = {
-    name: "negar",
-    age: 30,
+var combine = function (input1, input2) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 };
-console.log(person.name, person.age);
-//-------------------------------------------------------------//
-var product = {
-    id: "abc1",
-    price: 12.99,
-    tags: ["great-offer", "hot-and-new"],
-    numbers: [1, 2, 3],
-    data: [1, 2, "new", [3, "expensive"], { key: "On Sale" }],
-    role: [2, "author"],
-    details: {
-        title: "Red Carpet",
-        description: "A great carpet - almost brand-new!",
-    },
-};
-console.log(product.details.title);
-console.log(product.data[2]);
-//a simple for loop in an array of strings
-for (var _i = 0, _a = product.tags; _i < _a.length; _i++) {
-    var dat = _a[_i];
-    console.log(dat.toLocaleUpperCase()); //we can use methods for strings
-}
-//a simple for loop in an array of numbers
-for (var _b = 0, _c = product.numbers; _b < _c.length; _b++) {
-    var num = _c[_b];
-    console.log(num + typeof num + " converted to " + num.toString() + typeof num.toString());
-}
-//-------------------------------------------------------------//
-//working with Tuples
-product.role.push("writer"); //push method works with tuple! strange!
-console.log(product.role); //[2, 'author', 'writer']
-// product.role = [-5, 10]; //error
-// product.role[1] = 10;
-console.log(product.role); //[2, 10, "writer"];
+var combinedNumbers = combine(22, 44);
+console.log(combinedNumbers);
+var combinedNames = combine("abc", "def");
+console.log(combinedNames);
--- project-setup/app.ts
+++ project-setup/app.ts
@@ -1,67 +1,19 @@
-// const person: {
-//     name: string;
-//     age: number
-// } = {
-const person = {
-  name: "negar",
-  age: 30,
-};
-console.log(person.name, person.age);
+//union types
 
-//-------------------------------------------------------------//
+const combine = (input1: number | string, input2: number | string) => {
+  let result;
 
-const product: {
-  id: string;
-  price: number;
-  tags: string[];
-  numbers: number[];
-  data: (
-    | string
-    | number
-    | (string | number)[]
-    | {
-        key: string;
-      }
-  )[];
-  role: [number, string]; //its a tuple type!!!
-  details: {
-    title: string;
-    description: string;
-  };
-} = {
-  id: "abc1",
-  price: 12.99,
-  tags: ["great-offer", "hot-and-new"],
-  numbers: [1, 2, 3],
-  data: [1, 2, "new", [3, "expensive"], { key: "On Sale" }],
-  role: [2, "author"],
-  details: {
-    title: "Red Carpet",
-    description: "A great carpet - almost brand-new!",
-  },
+  //runtime type check
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 };
 
-console.log(product.details.title);
-console.log(product.data[2]);
+const combinedNumbers = combine(22, 44);
+console.log(combinedNumbers);
 
-//a simple for loop in an array of strings
-for (const dat of product.tags) {
-  console.log(dat.toLocaleUpperCase()); //we can use methods for strings
-}
-//a simple for loop in an array of numbers
-for (const num of product.numbers) {
-  console.log(
-    num + typeof num + " converted to " + num.toString() + typeof num.toString()
-  );
-}
-
-//-------------------------------------------------------------//
-
-//working with Tuples
-product.role.push("writer"); //push method works with tuple! strange!
-console.log(product.role); //[2, 'author', 'writer']
-
-// product.role = [-5, 10]; //error
-
-// product.role[1] = 10;
-console.log(product.role); //[2, 10, "writer"];
+const combinedNames = combine("abc", "def");
+console.log(combinedNames);
