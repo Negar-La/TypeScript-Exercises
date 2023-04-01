52b79532d7ea1b5e001248252983f38e246ea738
Negar Lashgari <negar.lashgari@gmail.com>
2023-03-31 22:48:00 -0400

    union-aliases

--- project-setup/app.js
+++ project-setup/app.js
@@ -1,14 +1,25 @@
-var combine = function (input1, input2) {
+var combine = function (input1, //union type
+input2, //union type
+resultConversion //a Literal type combined with union type
+) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    //runtime type check
+    if ((typeof input1 === "number" && typeof input2 === "number" || resultConversion === 'to-number')) {
+        result = +input1 + +input2; //add + to convert string to number so u can add "22" and "44" = 66
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
+    // if (resultConversion === 'to-number') {
+    //     return +result;
+    // } else {
+    //   return result.toString();
+    // }
 };
-var combinedNumbers = combine(22, 44);
-console.log(combinedNumbers);
-var combinedNames = combine("abc", "def");
+var combinedNumbers = combine(22, 44, "to-number");
+console.log(combinedNumbers, typeof combinedNumbers);
+var combinedStringNumbers = combine("22", "44", "to-number");
+console.log(combinedStringNumbers, typeof combinedStringNumbers);
+var combinedNames = combine("abc", "def", "to-text");
 console.log(combinedNames);
