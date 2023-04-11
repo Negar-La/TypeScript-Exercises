d43a6b3ce80a3077957dc760caa384d5767f80e7
Negar Lashgari <negar.lashgari@gmail.com>
2023-04-11 19:29:23 -0400

    class-private-public-readonly

--- classes-interfaces/dist/app.js
+++ classes-interfaces/dist/app.js
@@ -1,10 +1,34 @@
 "use strict";
 class Department {
-    constructor(n) {
-        this.name = n;
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+        //   name: string;
+        // private readonly  id: string;
+        this.employees = []; //only accessible within class 'Department'.
+        // this.name = n;
+    }
+    description() {
+        console.log(`Department (${this.id}): ${this.name}`);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInfo() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
-const xx = new Department('Accounting');
-console.log(xx);
-const yy = new Department('Sales');
-console.log(yy);
+const accounting = new Department("Accounting", "d1");
+console.log(accounting);
+accounting.description();
+accounting.addEmployee("max");
+accounting.addEmployee("manu");
+// accounting.employees[2] = "anna"
+accounting.printEmployeeInfo();
+// const sales = new Department("Sales");
+// console.log(sales);
+// const z = sales.description();
+// console.log(z); //This is the department of Sales
+// const acc = {description: x.description, name: 's'}
+// acc.description(); //This is the department of undefined
--- classes-interfaces/src/app.ts
+++ classes-interfaces/src/app.ts
@@ -1,15 +1,46 @@
 class Department {
-    name: string;
+//   name: string;
+// private readonly  id: string;
+  private employees: string [] =[]; //only accessible within class 'Department'.
 
-    constructor(n: string) {
-        this.name = n;
-    }
+  constructor(public name: string, private readonly id: string) {  //readonly means it shouldn't change
+    // this.name = n;
+  }
+
+  description(this: Department) {   //this refers to an instance based on Department class, so obj would be of type Department
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+    
+  }
 }
 
-const xx = new Department('Accounting');
-console.log(xx);
+const accounting = new Department("Accounting", "d1");
+console.log(accounting);
 
-const yy = new Department('Sales');
-console.log(yy);
+accounting.description();
+
+accounting.addEmployee("max")
+accounting.addEmployee("manu")
+// accounting.employees[2] = "anna"
+accounting.printEmployeeInfo()
+
+// const sales = new Department("Sales");
+// console.log(sales);
+
+// const z = sales.description();
+// console.log(z); //This is the department of Sales
+
+
+
+// const acc = {description: x.description, name: 's'}
+// acc.description(); //This is the department of undefined
 
 
