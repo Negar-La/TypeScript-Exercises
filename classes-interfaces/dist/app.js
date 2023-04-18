f41a772dc36c8dccf11c447460556a4527ff4073
Negar Lashgari <negar.lashgari@gmail.com>
2023-04-18 11:16:37 -0400

    comments

--- classes-interfaces/dist/app.js
+++ classes-interfaces/dist/app.js
@@ -1,11 +1,13 @@
 "use strict";
 class Department {
+    // private means only accessible within class 'Department'. private field or private property.
+    //private keyword is a modifier.
     constructor(name, id) {
         this.name = name;
         this.id = id;
-        //   name: string;
+        // public name: string;  public is default, you dont need to add it.
         // private readonly  id: string;
-        this.employees = []; //only accessible within class 'Department'.
+        this.employees = []; // protected is like private, accessible in this class and also in extended classes
         // this.name = n;
     }
     description() {
@@ -19,13 +21,45 @@ class Department {
         console.log(this.employees);
     }
 }
-const accounting = new Department("Accounting", "d1");
-console.log(accounting);
-accounting.description();
-accounting.addEmployee("max");
-accounting.addEmployee("manu");
-// accounting.employees[2] = "anna"
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        super('IT', id); //super calls the constructor of the base class
+        this.admins = admins;
+    }
+}
+class AccountingDepartment extends Department {
+    constructor(id, reports) {
+        super('Accounting', id);
+        this.reports = reports;
+    }
+    addEmployee(name) {
+        if (name === 'max') {
+            return;
+        }
+        this.employees.push(name); //because employees is protected, not private.
+    }
+    addReports(text) {
+        this.reports.push(text);
+    }
+    printReports() {
+        console.log(this.reports);
+    }
+}
+const it = new ITDepartment("d1", ['max']);
+console.log(it);
+it.description();
+it.name = 'NEW NAME'; //ecause name is public!
+it.addEmployee("max");
+it.addEmployee("manu");
+// it.employees[2] = "anna"
+it.printEmployeeInfo();
+const accounting = new AccountingDepartment("d2", []);
+accounting.addReports('something went wrong...');
+accounting.printReports();
+accounting.addEmployee('max'); // we see 'max' is not added.
+accounting.addEmployee('neg');
 accounting.printEmployeeInfo();
+console.log(accounting);
 // const sales = new Department("Sales");
 // console.log(sales);
 // const z = sales.description();
--- classes-interfaces/src/app.ts
+++ classes-interfaces/src/app.ts
@@ -1,13 +1,15 @@
 class Department {
-//   name: string;
+// public name: string;  public is default, you dont need to add it.
 // private readonly  id: string;
-  private employees: string [] =[]; //only accessible within class 'Department'.
+  protected employees: string [] =[]; // protected is like private, accessible in this class and also in extended classes
+  // private means only accessible within class 'Department'. private field or private property.
+  //private keyword is a modifier.
 
-  constructor(public name: string, private readonly id: string) {  //readonly means it shouldn't change
+  constructor(public name: string, private readonly id: string) {  //add modifier in front of parameters. readonly means it shouldn't change
     // this.name = n;
   }
 
-  description(this: Department) {   //this refers to an instance based on Department class, so obj would be of type Department
+  description(this: Department) {   //this in line 11 should refer to an instance based on Department class, so obj would be of type Department
     console.log(`Department (${this.id}): ${this.name}`);
   }
 
@@ -18,19 +20,58 @@ class Department {
   printEmployeeInfo() {
     console.log(this.employees.length);
     console.log(this.employees);
+  }
+}
+
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins:string[]){
+    super('IT', id);  //super calls the constructor of the base class
+    this.admins = admins;
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]){
+    super('Accounting', id)
+  }
+
+  addEmployee(name: string) {   //overriding methods of the base class
+    if (name === 'max') {
+      return;
+    }
+    this.employees.push(name) //because employees is protected, not private.
+  }
+
+  addReports(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
     
   }
 }
 
-const accounting = new Department("Accounting", "d1");
+const it = new ITDepartment ("d1", ['max']);
+console.log(it);
+
+it.description();
+it.name = 'NEW NAME' //ecause name is public!
+it.addEmployee("max")
+it.addEmployee("manu")
+// it.employees[2] = "anna"
+it.printEmployeeInfo()
+
+
+const accounting = new AccountingDepartment ("d2", []);
+accounting.addReports('something went wrong...');
+accounting.printReports()
+accounting.addEmployee('max') // we see 'max' is not added.
+accounting.addEmployee('neg')
+accounting.printEmployeeInfo()
 console.log(accounting);
 
-accounting.description();
-
-accounting.addEmployee("max")
-accounting.addEmployee("manu")
-// accounting.employees[2] = "anna"
-accounting.printEmployeeInfo()
 
 // const sales = new Department("Sales");
 // console.log(sales);
